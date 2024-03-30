6a070683c0aece81ee7acf8a4b9c911eaa6b9057
AbhinavOhri <abhinavohri13@gmail.com>
2024-03-30 10:49:37 +0530

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import IconTray from './components/IconTray.tsx'
+import Navbar from "./components/Navbar.tsx"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <IconTray/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/IconTray.tsx
@@ -0,0 +1,57 @@
+import about from "../assets/about_grey.svg"
+const IconTray = () => {
+    return (
+        <div className="container-icon-tray">
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>About IstSos</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>GetObservation Configuration</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+            <div className="sub-menu-item">
+                <img src={about} />
+                <div>jjj</div>
+            </div>
+        </div>
+    )
+}
+
+export default IconTray
