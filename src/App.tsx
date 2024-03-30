0322accf547b6d94362238f01b13af627d4282e5
AbhinavOhri <abhinavohri13@gmail.com>
2024-03-30 22:21:38 +0530

    Setting up routes for icons on tcon tray

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
-import IconTray from './components/IconTray.tsx'
-import Navbar from "./components/Navbar.tsx"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Ensure correct imports
+import './App.css';
+import IconTray from './components/IconTray.tsx';
+import Navbar from "./components/Navbar.tsx";
+import About from './components/util/About.tsx';
+import Database from './components/util/Database.tsx';
+import ServiceProvider from './components/util/ServiceProvider.tsx';
+import ServiceIdentification from './components/util/ServiceIdentification.tsx';
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <IconTray/>
+    <BrowserRouter>
+      <Navbar />
+      <IconTray /> 
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/database" element={<Database/>} />
+        <Route path="/serviceprovider" element={<ServiceProvider/>} />
+        <Route path="/serviceidentification" element={<ServiceIdentification/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
