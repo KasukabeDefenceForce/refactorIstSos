3a1ef14b67bcf2083b06f6288008428346afb111
AbhinavOhri <abhinavohri13@gmail.com>
2024-03-31 00:28:43 +0530

    Add form on proxy config page

--- src/components/Pages/ProxyConfig.tsx
+++ src/components/Pages/ProxyConfig.tsx
@@ -1,7 +1,32 @@
+import { Box } from "@mui/material"
+import { useState } from "react"
+import InputBoxComponent from "../util/InputBoxComponent"
 
 const ProxyConfig = () => {
+  const [url, setUrl] = useState("http://localhost/istsos")
+  const handleClick = () => {
+    const sanitizedMessage = `
+      URL: ${url}
+    `;
+    alert(sanitizedMessage);
+  };
   return (
-    <div>ProxyConfig</div>
+    <>
+      <div className="page-name">
+        <Box sx={{ flexGrow: 1 }}>
+          <strong>default &gt; Proxy Configuration</strong>
+        </Box>
+      </div>
+      <div className="about">
+        <div className="floating-label">Proxy URL</div>
+        <form onSubmit={handleClick}>
+          <InputBoxComponent label="User" type="text" setValue={setUrl} value={url} />
+          <div className="button-container">
+            <button className="submit-button">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
