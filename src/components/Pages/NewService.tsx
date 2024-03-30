3a5158649741ce15244f9dc60efc84928242270d
AbhinavOhri <abhinavohri13@gmail.com>
2024-03-31 00:40:52 +0530

    Adding content on new service page

--- src/components/Pages/NewService.tsx
+++ src/components/Pages/NewService.tsx
@@ -1,7 +1,21 @@
+import { Box } from "@mui/material"
 
 const NewService = () => {
   return (
-    <div>NewService</div>
+    <>
+      <div className="page-name">
+        <Box sx={{ flexGrow: 1 }}>
+          <strong>&gt; Initialize a new service</strong>
+        </Box>
+      </div>
+      <div>
+        <p>This proceduce will guide you in each step needed to configure a new Sensor Observation Service.</p>
+        <p>Please follow the setup instruction to start using it.</p>
+      </div>
+      <div className="button-container">
+        <button className="submit-button">Next</button>
+      </div>
+    </>
   )
 }
 
